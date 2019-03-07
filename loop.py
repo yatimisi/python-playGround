8290bf2601615ecb4e3b64768fb2884803e03879
Yatimisi <yatimisi2012@gmail.com>
2019-03-07 16:44:13 +0800

    while

--- loop.py
+++ loop.py
@@ -6,6 +6,12 @@ for n in numbers:
 for i in range(len(numbers)):
     print(numbers[i])
 
+i = 0
+while i < len(numbers):
+    print(numbers[i])
+    i += 1
+
+
 # print(numbers[2])
 # print(numbers[2:5])
 # print(numbers[:5])
