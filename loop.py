bc184cb331f77ded1ab55adcf2f8e4b17ac17556
Yatimisi <yatimisi2012@gmail.com>
2019-03-07 16:41:41 +0800

    for

--- loop.py
+++ loop.py
@@ -1,8 +1,13 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-print(numbers[2])
-print(numbers[2:5])
-print(numbers[:5])
-print(numbers[2:])
-print(numbers[::2])
-print(numbers[::-1])
+for n in numbers:
+    print(n)
+
+    
+
+# print(numbers[2])
+# print(numbers[2:5])
+# print(numbers[:5])
+# print(numbers[2:])
+# print(numbers[::2])
+# print(numbers[::-1])
